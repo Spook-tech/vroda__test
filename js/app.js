759d550aa43804856916d9e3a2540110d76fe35a
Kostya Bondarenko <61065186+Spook-tech@users.noreply.github.com>
2023-02-26 16:44:48 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const screenSize = window.screen.width;
 
+
 document.body.addEventListener('click', (e) => {
   const target = e.target;
   
@@ -12,6 +13,10 @@ document.body.addEventListener('click', (e) => {
 const swiper = new Swiper('.reviews-swiper', {
   spaceBetween: 30,
 
+  navigation: {
+    nextEl: '.reviews-button-next',
+    prevEl: '.reviews-button-prev',
+  },
 
   breakpoints: {
     // when window width is >= 320px
@@ -25,4 +30,29 @@ const swiper = new Swiper('.reviews-swiper', {
       spaceBetween: 40
     }
   }
+});
+
+var lastScrollTop = 0;
+
+const tonicImg = document.querySelector('.tonic img');
+let imgPosition = 0;
+window.addEventListener("scroll", function() {
+  const scrollSpread = 15;
+
+  var st = window.pageYOffset || document.documentElement.scrollTop;
+  if (st > lastScrollTop) {
+    // Скролл вниз
+    imgPosition++;
+
+    imgPosition = imgPosition > scrollSpread ? 0 : imgPosition;
+  } else if (st < lastScrollTop) {
+    // Скролл вверх
+    imgPosition--;
+
+    imgPosition = imgPosition < -scrollSpread ? 0 : imgPosition;
+  } 
+  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+
+  tonicImg.style.transform = `translate3d(0, ${imgPosition}px, 0)`;
+
 });
