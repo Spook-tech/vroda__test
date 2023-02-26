a548e3d94f758949bb27820921d53587d25fb7bf
Kostya Bondarenko <61065186+Spook-tech@users.noreply.github.com>
2023-02-26 17:29:51 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const screenSize = window.screen.width;
-
+const line = document.querySelector('#line');
 
 document.body.addEventListener('click', (e) => {
   const target = e.target;
@@ -36,23 +36,44 @@ var lastScrollTop = 0;
 
 const tonicImg = document.querySelector('.tonic img');
 let imgPosition = 0;
+let animated = false;
 window.addEventListener("scroll", function() {
+  let scrolled = window.pageYOffset;
   const scrollSpread = 15;
-
-  var st = window.pageYOffset || document.documentElement.scrollTop;
+  
+  let st = window.pageYOffset || document.documentElement.scrollTop;
   if (st > lastScrollTop) {
     // Скролл вниз
     imgPosition++;
-
+    
     imgPosition = imgPosition > scrollSpread ? 0 : imgPosition;
   } else if (st < lastScrollTop) {
     // Скролл вверх
     imgPosition--;
-
+    
     imgPosition = imgPosition < -scrollSpread ? 0 : imgPosition;
   } 
   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-
+  
   tonicImg.style.transform = `translate3d(0, ${imgPosition}px, 0)`;
+  
 
+  if (!animated){
+    if (offset(line).top-1000 <= scrolled){
+      new Vivus('line', {start: 'autostart', type: 'oneByOne', duration: 1500});
+      animated = true;
+    }  
+  }
 });
+
+// Анимация при скролле
+let elements = document.querySelectorAll(".to__anim");
+let html = document.querySelector('html');
+
+
+function offset(el) {
+  var rect = el.getBoundingClientRect(),
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
